5b0364cbe7b75502cb81c19745192ee30d0720d0
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-03-05 08:58:28 +0900

    student登録
    トップページ編集

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,20 @@
+class StudentsController < ApplicationController
+  def new
+    @student = Student.new
+  end
+  def create
+    @student = Student.new(student_params)
+    if @student.valid?
+      @student.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+  def student_params
+    params.require(:student).permit(:name,:memo).merge(user_id: current_user.id)
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, only: [:index]
+  resources :students, only: [:new, :create]
 
   root 'groups#index'
 end
