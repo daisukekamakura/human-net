ac7e7d135beacd642aa4c35cf21249c1367cf7a4
daisukekamakura <74848910+daisukekamakura@users.noreply.github.com>
2021-03-05 15:30:42 +0900

    MVC作成
    ７つのアクション完成

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -11,9 +11,29 @@ class StudentsController < ApplicationController
       render :new
     end
   end
-  
+
   def show
     @student = Student.find(params[:id])
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+    @student.update(student_params)
+    if @student.update(student_params)
+      redirect_to student_path(@student) 
+    else
+      render :edit        
+    end
+  end
+
+  def destroy
+    @student = Student.find(params[:id])
+    @student.destroy
+    redirect_to root_path(@student) 
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :groups, only: [:index, :show]
-  resources :students, only: [:new, :create, :show]
+  resources :students, only: [:new, :create, :show, :edit, :update, :destroy]
 
   root 'groups#index'
 end
